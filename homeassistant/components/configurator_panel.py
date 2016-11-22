254a018d85b143e471357cb397458fffe7b61855
geekman2 <geekmann2@gmail.com>
2016-11-21 19:59:22 -0800

    Registered new API endpoint first try

--- homeassistant/components/configurator_panel.py
+++ homeassistant/components/configurator_panel.py
@@ -1,10 +1,63 @@
 import asyncio
 from collections import defaultdict
+from datetime import timedelta
+from itertools import groupby
+import voluptuous as vol
+from homeassistant.const import HTTP_BAD_REQUEST
+import homeassistant.helpers.config_validation as cv
+import homeassistant.util.dt as dt_util
 from homeassistant.components import recorder, script
+from homeassistant.components.frontend import register_built_in_panel
+from homeassistant.components.http import HomeAssistantView
+from homeassistant.const import ATTR_HIDDEN
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.components.history import get_unique_states
 import requests
 
+DOMAIN = 'history'
+DEPENDENCIES = ['recorder', 'http']
+
+CONF_EXCLUDE = 'exclude'
+CONF_INCLUDE = 'include'
+CONF_ENTITIES = 'entities'
+CONF_DOMAINS = 'domains'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        CONF_EXCLUDE: vol.Schema({
+            vol.Optional(CONF_ENTITIES, default=[]): cv.entity_ids,
+            vol.Optional(CONF_DOMAINS, default=[]):
+                vol.All(cv.ensure_list, [cv.string])
+        }),
+        CONF_INCLUDE: vol.Schema({
+            vol.Optional(CONF_ENTITIES, default=[]): cv.entity_ids,
+            vol.Optional(CONF_DOMAINS, default=[]):
+                vol.All(cv.ensure_list, [cv.string])
+        })
+    }),
+}, extra=vol.ALLOW_EXTRA)
+
+SIGNIFICANT_DOMAINS = ('thermostat', 'climate')
+IGNORE_DOMAINS = ('zone', 'scene',)
+
+
+def setup(hass, config):
+    """Setup the panel hooks."""
+    filters = Filters()
+    exclude = config[DOMAIN].get(CONF_EXCLUDE)
+    if exclude:
+        filters.excluded_entities = exclude[CONF_ENTITIES]
+        filters.excluded_domains = exclude[CONF_DOMAINS]
+    include = config[DOMAIN].get(CONF_INCLUDE)
+    if include:
+        filters.included_entities = include[CONF_ENTITIES]
+        filters.included_domains = include[CONF_DOMAINS]
+
+    hass.http.register_view(UniqueStatesView(hass))
+
+
+
+    return True
 
 def get_known_states(hass, entity_id=None, api_url='state', api_password=''):
     """Return the last 5 states for entity_id."""
@@ -23,6 +76,7 @@ def get_known_states(hass, entity_id=None, api_url='state', api_password=''):
     else:
         result = get_unique_states(hass, entity_id=entity_id)
     return result
+
 
 class UniqueStatesView(HomeAssistantView):
     """Handle unique states view requests."""
